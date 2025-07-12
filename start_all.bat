772ec97e827c6d2bb1867df198ecadd7c569a227
Niranjan Reddy <83489863+jniranjanreddy@users.noreply.github.com>
2025-07-12 01:01:23 -0400

    Update start_all.bat

--- start_all.bat
+++ start_all.bat
@@ -1,3 +1,3 @@
 start cmd /k "cd express-app && node app.js"
-start cmd /k "cd flask-app && python app.py
+start cmd /k "cd flask-app && python app.py"
 start cmd /k "cd fastapi-app && uvicorn main:app --reload --port 8000"
