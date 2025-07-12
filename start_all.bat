2a6a3fee32d7d8b56e74e6ce1981fa4262a1115f
Niranjan Reddy <Niranjan.jannapureddy@rhythmx.ai>
2025-07-12 00:59:36 -0400

    addd

--- start_all.bat
+++ start_all.bat
@@ -1,3 +1,3 @@
 start cmd /k "cd express-app && node app.js"
-start cmd /k "cd flask-app && venv\Scripts\activate && python app.py"
-start cmd /k "cd fastapi-app && venv\Scripts\activate && uvicorn main:app --reload --port 8000"
+start cmd /k "cd flask-app && python app.py
+start cmd /k "cd fastapi-app && uvicorn main:app --reload --port 8000"
